4c435613158fa8484e53ae28fca74038777432c9
RamPageMz <1033516561@qq.com>
2018-04-21 19:55:47 +0800

    增加了jpa依赖/新增user类

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,4 +1,65 @@
 package com.example.demo.domain;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialID=1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = true)
+    private Integer age;
+
+
+    protected User(){
+
+    }
+
+    public User(String username,String password){
+        this.username=username;
+        this.password=password;
+    }
+
+
+    /*
+    *   getter setter
+    * */
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
